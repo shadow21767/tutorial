3d7bc89bbdcb69101855a8abd8c13af7047d76ce
shadow21767 <pujanp21@gmail.com>
2023-02-04 17:53:48 -0500

    Currently working on beginner-level Python

---
+++ python.py
@@ -0,0 +1,21 @@
+# Comments: are used to ignore a part of a program, provide context why something is written the way it is.
+
+#print(): tell a computer to talk
+print("Hello World")
+
+#Strings: blocks of text surrounded by double quotes or single quotes
+print("Gh0st141")
+print('Gh0st141')
+
+#Variables: method of storing data for reuse
+planet_name = "Mars"
+print(planet_name)
+        #Variable: numbers don't need quotes
+planet_number = 4
+print(planet_number)
+
+#Integer (int) is a whole number. No decimal point and contains all counting numbers, even negative numbers
+#Floating-point number (float) is a decimal number. Used to represent fractional quantities as well as precise measurements
+an_int = 2
+a_float = 2.1
+print(an_int + 3)
